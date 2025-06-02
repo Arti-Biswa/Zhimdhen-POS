85d4943cbe968267a169f510404e83bc01cd0024
Arti-Biswa <aratibiswa481@gmail.com>
2025-06-02 16:10:36 +0600

    POS-01:Project Initialization.

---
+++ src/test/java/com/java/Zhimdhen_POS/ZhimdhenPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.Zhimdhen_POS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhimdhenPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
